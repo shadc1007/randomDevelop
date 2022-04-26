8142058bcf0c6049b5640feae070e57f14a5fa55
Shadrack V. Castro <castroshadrack7@gmail.com>
2022-04-26 22:41:37 +0800

    added challenge 4 todo list app

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const soundsBtn = document.querySelector("#sounds");
 const genCat = document.querySelector("#genCat");
 const resetCat = document.querySelector("#resetCat");
 const catDiv = document.querySelector(".cat-image");
+const inputText = document.querySelector("#textarea");
+const plusButton = document.querySelector("#todoBtn");
+const todoArea = document.querySelector(".todo-area");
+
 
 // Event listeners 
 greenButton.addEventListener('click', buttonFun);
@@ -17,6 +21,9 @@ quoteButton.addEventListener('click', newQuote);
 soundsBtn.addEventListener('click', soundsFun);
 genCat.addEventListener('click', generateCat);
 resetCat.addEventListener('click', removeCat);
+plusButton.addEventListener('click', addTask);
+todoArea.addEventListener('click', removeList)
+
 
 // function 
 function buttonFun() {
@@ -82,3 +89,48 @@ function removeCat() {
     catDiv.removeChild(catDiv.firstElementChild);
 
 }
+
+// Challenge 4 todo list
+function addTask(e) {
+    // prevent from refreshing
+    e.preventDefault();
+    // input value
+    const inputValue = inputText;
+    // Creating todo div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    // Creating li 
+    const todoList = document.createElement("li");
+    todoList.innerText = inputValue.value
+    todoList.classList.add("list");
+    todoDiv.appendChild(todoList);
+    // creating check button
+    const checkButton = document.createElement("button");
+    checkButton.classList.add("check-btn");
+    checkButton.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    todoDiv.appendChild(checkButton);
+    // Creating delete button
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-btn");
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    deleteButton.setAttribute('id', 'deleteId');
+    todoDiv.appendChild(deleteButton); 
+    // Append
+    todoArea.appendChild(todoDiv);
+    // reset input value
+    inputValue.value = '';
+};
+
+// Delete list
+function removeList(e) {
+    const target = e.target;
+    if (target.classList.contains("delete-btn")) {
+        const parentEl = target.parentElement;
+        parentEl.remove();
+    }
+
+    if (target.classList.contains("check-btn")) {
+        const completed = target.parentElement;
+        completed.classList.toggle("completed");
+    }
+};
